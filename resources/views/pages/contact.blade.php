cc6fe7c9e6a052117eaed06f287837d1747f7e1d
Tri Aldy Kurniawan <122498697+Kenji2005@users.noreply.github.com>
2023-06-23 10:40:07 +0700

    heading contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="bg-primary-black h-[344px] w-full">
-        
+    <div class="bg-primary-black text-primary-silver h-[344px] w-full flex justify-center items-center">
+        <div class="text-center">
+            <h1 class="mb-6 text-6xl font-bold">Contact Us</h1>
+            <h3 class="text-lg">We value every feedback from our customers. Do you have any suggestions, feedback or experiences you'd like to share</h3>
+        </div>
     </div>
 @endsection
