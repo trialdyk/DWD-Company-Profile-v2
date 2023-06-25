d0add8e93cdac261c6f2f5d0a3906910d1e91852
mohamadarif03 <pecintarealmadrid2@gmail.com>
2023-06-25 10:02:55 +0700

    product

---
+++ resources/views/pages/product.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Head -->
+    <section class="bg-primary-black text-primary-silver h-[344px] py-[2.5rem] pl-[65px] w-full">
+        <div class="grid grid-cols-2 gap-6">
+            <div class="">
+                <h1 class="mb-6 text-6xl w-[70%] font-bold">Product</h1>
+                <h3 class="text-lg w-[70%]">We are specialized in designing kitchens, wardrobes, Walking closets,</h3>
+            </div>
+            <div class="relative">
+                <div class="bg-primary-silver text-black w-[14rem] h-[7rem] rounded-md border-2 border-indigo-600">
+                    test
+                </div>
+                <div class="absolute top-0 left-[16rem]">
+                    <div class="bg-primary-silver text-black w-[7rem] h-[7rem] rounded-md border-2 border-indigo-600">
+                        test
+                    </div>
+                </div>
+                <div class="absolute top-[8.5rem] left-[6rem]">
+                    <div class="bg-primary-silver text-black w-[14rem] h-[7rem] rounded-md border-2 border-indigo-600">
+                        test
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--End Head -->
+
+    <!-- Content -->
+    <section>
+        
+    </section>
+    <!-- End Content -->
+@endsection
